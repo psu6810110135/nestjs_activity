2611a157f4163042d63320556a5ccc5c9e5fbb72
ThanaphiphatJansuwan <404tkth@gmail.com>
2025-12-17 10:18:06 +0700

    fix bug phase 3

---
+++ src/book-category/book-category.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { BookCategoryService } from './book-category.service';
+import { BookCategoryController } from './book-category.controller';
+import { BookCategory } from './entities/book-category.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [TypeOrmModule.forFeature([BookCategory])],
+  controllers: [BookCategoryController],
+  providers: [BookCategoryService],
+})
+export class BookCategoryModule {}
---
+++ src/book-category/book-category.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@nestjs/common';
+import { CreateBookCategoryDto } from './dto/create-book-category.dto';
+import { UpdateBookCategoryDto } from './dto/update-book-category.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { BookCategory } from './entities/book-category.entity';
+
+@Injectable()
+export class BookCategoryService {
+  constructor(
+    @InjectRepository(BookCategory)
+    private repo: Repository<BookCategory>,
+  ) {}
+  async onModuleInit() {
+    const count = await this.repo.count();
+    if (count === 0) {
+      console.log('Seeding Book Categories...');
+      await this.repo.save([
+        { name: 'Fiction', description: 'Stories and novels' },
+        { name: 'Technology', description: 'Computers and engineering' },
+        { name: 'History', description: 'Past events' },
+      ]);
+    }
+  }
+
+  create(createBookCategoryDto: CreateBookCategoryDto) {
+    return this.repo.save(createBookCategoryDto);
+  }
+
+  findAll() {
+    return this.repo.find();
+  }
+  findOne(id: string) {
+    return this.repo.findOneBy({ id });
+  }
+
+  update(id: number, updateBookCategoryDto: UpdateBookCategoryDto) {
+    return this.repo.update(id, updateBookCategoryDto);
+  }
+
+  remove(id: number) {
+    return this.repo.delete(id);
+  }
+}
